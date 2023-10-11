757dc34ed30837414f24284ed6442b071b285d4d
olliepegram <olliepegram@gmail.com>
2023-10-11 12:30:31 +1100

    more work on program logic

--- src/Program.js
+++ src/Program.js
@@ -23,16 +23,54 @@ const calculateHeartRateZones = (minHeartRate, maxHeartRate) => {
 	return heartRateZones;
 };
 
+const generateRunningProgram = (
+	currentWeeklyDistance,
+	desiredRunningDistance,
+	runningDaysPerWeek
+) => {
+	const weeklyDistanceIncreasePercentage = 0.1;
+	const weeklySpeed = 0.2 * currentWeeklyDistance;
+	const longRunDay = 0.4 * currentWeeklyDistance;
+	const easyRuns = currentWeeklyDistance - weeklySpeed - longRunDay;
+	const daysPerWeek = 7;
+
+	let currentWeek = 1;
+	const schedule = [];
+
+	while (currentWeeklyDistance < desiredRunningDistance) {
+		for (let i = 0 + 1; i < daysPerWeek; i++) {}
+
+		currentWeeklyDistance +=
+			weeklyDistanceIncreasePercentage * currentWeeklyDistance;
+		currentWeek++;
+	}
+	return schedule;
+};
+
 function Program({ user }) {
 	const { minHeartRate, maxHeartRate } = user;
 	const heartRateZones = calculateHeartRateZones(minHeartRate, maxHeartRate);
 	const catagories = {
-		easy: heartRateZones['Zone 2'],
-		temp: maxHeartRate * 0.87,
-		speed: maxHeartRate * 0.94,
+		easy: { heartRate: heartRateZones['Zone 2'], difficulty: 'easy' },
+		temp: { heartRate: maxHeartRate * 0.87, difficulty: 'hard' },
+		speed: { heartRate: maxHeartRate * 0.94, difficulty: 'hard' },
+		long: { heartRate: heartRateZones['Zone 2'], difficulty: 'hard' },
 	};
 
-	console.log(Math.round(catagories.temp));
+	// Example usage
+	const currentWeeklyDistance = 10; // Replace with the starting weekly distance in km
+	const desiredWeeklyDistance = 30; // Replace with the desired weekly distance in km
+	const runningDaysPerWeek = 4; // Replace with the number of running days per week
+
+	const program = generateRunningProgram(
+		currentWeeklyDistance,
+		desiredWeeklyDistance,
+		runningDaysPerWeek
+	);
+
+	// Output the generated running program
+	console.log('Running Program:');
+	program.forEach((day) => console.log(day));
 
 	return <div>Program</div>;
 }
