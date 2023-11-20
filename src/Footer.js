7b5120745831c88dc65005dfd9f5cd71e3055bce
olliepegram <olliepegram@gmail.com>
2023-11-21 06:20:09 +1100

    footer work

--- src/Footer.js
+++ src/Footer.js
@@ -3,15 +3,33 @@ import arrow from './arrow.png';
 
 function Footer() {
 	return (
-		<div className={styles.footer}>
-			<div>
-				{/* <img
-					src={arrow}
-					className={styles.arrow}
-					alt={'pic of stairs/logo'}
-				/> */}
+		<div className={styles.footerContainer}>
+			<div className={styles.footer}>
+				<div>
+					{/* <img
+						src={arrow}
+						className={styles.arrow}
+						alt={'pic of stairs/logo'}
+					/> */}
+
+					<h3>RunRampUp</h3>
+				</div>
+				<div>
+					{' '}
+					<img
+						src={arrow}
+						className={styles.arrow}
+						alt={'pic of stairs/logo'}
+					/>
+				</div>
+				<div className={styles.footerText}>
+					<p>About</p>
+					<p>
+						<a href='mailto:olliepegram@gmail.com'>Contact Us</a>
+					</p>
+					<p>Hire Me?</p>
+				</div>
 			</div>
-			<p>Made by Ollie</p>
 		</div>
 	);
 }
